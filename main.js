63c71cf246422e75643b15884151324f473eb929
David Grieser <david.grieser@galvanize.com>
2021-08-04 01:01:11 -0500

    baseline example

--- main.js
+++ main.js
@@ -1,46 +1,73 @@
 let promises = [];
+let questionCounter = 0;
 const elapsedTime = (t1, t2) => {
     return t2 - t1;
 }
 const createQuestion = () => {
-    const article = document.createElement('article')
-    article.class
-
+    questionCounter++;
+    const article = document.createElement('article');
+    article.classList.add('question');
+    article.setAttribute('id', `question${questionCounter}`)
+    const questionText = document.createElement('span');
+    questionText.innerText = `Question ${questionCounter}`;
+    const yesButton = document.createElement('button');
+    yesButton.classList.add('yes');
+    yesButton.innerText = 'Yes';
+    const noButton = document.createElement('button');
+    noButton.classList.add('no');
+    noButton.innerText = 'No';
+    const questionResult = document.createElement('span');
+    questionResult.classList.add('result')
+    article.appendChild(questionText);
+    article.appendChild(yesButton);
+    article.appendChild(noButton);
+    article.appendChild(questionResult);
+    return article;
 }
 
 const createPromise = () => {
+    const question = createQuestion();
+    document.querySelector('.questions').appendChild(question);
     return new Promise((resolve, reject) => {
         const startTime = new Date();
         if (document.querySelector('#throwToggle').checked) {
             throw "What do you mean you don't know?";
         }
-        document.querySelector('.yes').addEventListener('click', () => {
-            const result = { timeTaken: elapsedTime(startTime, new Date()), answer: true }
+        question.querySelector('.yes').addEventListener('click', () => {
+            const result = { timeTaken: elapsedTime(startTime, new Date()), answer: true, id: question.getAttribute('id') }
             resolve(result);
         });
-        document.querySelector('.no').addEventListener('click', () => {
-            const result = { timeTaken: elapsedTime(startTime, new Date()), answer: false }
+        question.querySelector('.no').addEventListener('click', () => {
+            const result = { timeTaken: elapsedTime(startTime, new Date()), answer: false, id: question.getAttribute('id')  }
             reject(result);
         })
     });
 
 }
+
+const processThen = (success) => {
+    document.getElementById(success['id']).querySelector('.result').innerText = success['timeTaken']
+    const resultElement = document.getElementById(success['id']).querySelector('.result')
+    resultElement.innerText = success['timeTaken']
+    resultElement.classList.add('success');
+}
+
 document.body.querySelector('#createPromise').addEventListener('click', () => {
     console.log("Promise Created")
     let promise = createPromise();
     console.log(promise);
-    promise.then((successMessage) => {
-        console.log(promise);
-        console.log(successMessage);
-    }).then((result) => {
-        console.info(`Before Catch: <${result}>`);
+    promise.then((success) => {
+        processThen(success)
     }).catch((error) => {
-        console.log(promise);
-        console.error(error)
-    }).then(() => {
-        console.info("Extra Call")
+        if(error['name'] === 'Error') {
+            console.error(error)
+        } else {
+            const resultElement = document.getElementById(error['id']).querySelector('.result')
+            resultElement.innerText = error['timeTaken']
+            resultElement.classList.add('error');
+        }
     }).finally(() => {
-        console.log(promise);
+        console.log(promise)
         console.log('Promise Finished');
     });
 })
