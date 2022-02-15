48ddca28e33f3bc0a4ad61b1d569682d57f0d3b7
David Grieser <david.grieser@galvanize.com>
2022-02-15 12:05:26 -0600

    drop promise chain

--- main.js
+++ main.js
@@ -113,7 +113,6 @@ const createManualPromise = () => {
 document.body.querySelector('#createPromise').addEventListener('click', () => {
     const promiseMethod = document.querySelector('#automatedPromise').checked ? createAutomatedPromise : createManualPromise;
     let promise = promiseMethod();
-    let promise2;
 
     console.log("Promise Created")
     console.log(promise);
@@ -121,14 +120,8 @@ document.body.querySelector('#createPromise').addEventListener('click', () => {
     promise.then((success) => {
         processSuccess(success);
         console.log('First Then:', promise);
-        promise2 = promiseMethod();
-        return promise2;
-    }).then((success) => {
-        processSuccess(success);
-        console.log('Second Then:', promise2);
     }).catch((error) => {
         console.log('Catch 1: ', promise);
-        console.log('Catch 2: ', promise2);
         console.log('Error: ', error);
         if(error['name'] === 'Error') {
             console.error(error);
